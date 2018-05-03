bab63ed94e2e9cc504d6389b13377387f0919ee8
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-05-03 15:00:01 +0200

    Change TodoForm to presentation component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import style from './TodoForm.css';
-class TodoForm extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    handleAddTask = () => {
-        const input = document.getElementById('addTask');
-        if(!input.value.length) {
-            input.placeholder='Set your task';
-        }
-        else {
-            this.props.add(input.value);
-            input.value="";
-        }
-    }
-    render() {
-        return (
-            <form onSubmit={(e)=>e.preventDefault()}>
-                <input className={style.TodoInput} type="text" name="task" id="addTask" placeholder='type your task...'/>
-                <button type="submit" className={style.TodoSubmit} onClick={this.handleAddTask}>Add task</button>
-            </form>);
-    }
+const TodoForm = (props) => {
+    return <form onSubmit={(e) =>{e.preventDefault(); props.add(e.target.firstChild.value)}}>
+        <input className={style.TodoInput} type="text" name="task" placeholder='type your task...' />
+        <button type="submit" className={style.TodoSubmit}>Add task</button>
+        </form>
 }
 export default TodoForm;
